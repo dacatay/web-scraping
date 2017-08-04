08cd9c1f3609ddeba0f076e0b50bd032f4698a41
Daniel K Acatay <daniel.acatay@googlemail.com>
2017-08-04 17:15:44 +0200

    updated

---
+++ web-scrapping.py
@@ -0,0 +1,28 @@
+import bs4 as bs
+import urllib.request
+
+
+
+
+#print(soup.prettify())
+
+
+
+
+nav = soup.nav
+#for url in nav.find_all('a'):
+#    print(url.get('href'))
+
+
+
+
+
+
+#TABLES AND XML
+
+
+
+
+
+
+
